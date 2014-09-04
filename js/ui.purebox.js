7a78e2cb51f835a60e656db8581c4ee6a9697876
Minwe <minwe@yunshipei.com>
2014-09-04 12:27:47 +0800

    add purebox plugin

---
+++ js/ui.purebox.js
@@ -0,0 +1,19 @@
+define(function(require, exports, module) {
+
+    require('core');
+
+    var $ = window.Zepto,
+        UI = $.AMUI;
+
+    var PureBox = function(element, options) {
+
+    };
+
+    PureBox.prototype.init = function() {
+
+    };
+
+    UI.purebox = PureBox;
+
+    module.exports = PureBox;
+});

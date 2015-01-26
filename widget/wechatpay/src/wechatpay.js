f2d3f67d7e897331511092fa9882019efebd27f4
Minwe <minwe@yunshipei.com>
2015-01-26 10:58:40 +0800

    WeChat Pay review

--- widget/wechatpay/src/wechatpay.js
+++ widget/wechatpay/src/wechatpay.js
@@ -1,48 +1,51 @@
+'use strict';
+
 var $ = require('jquery');
 require('./core');
 
-function addPayApi(callback) {
-  var $payApi = $('<script/>', {
-    id: 'am-pay-api'
+function appendWeChatSDK(callback) {
+  var $weChatSDK = $('<script/>', {
+    id: 'wechat-sdk'
   });
 
-  $('body').append($payApi);
+  $('body').append($weChatSDK);
 
-  $payApi.on('load', function() {
-    var script = document.createElement('script');
-    script.textContent = '(' + callback.toString() + ')();';
-    $('body')[0].appendChild(script);
+  $weChatSDK.on('load', function() {
+    callback && callback();
   }).attr('src', 'http://res.wx.qq.com/open/js/jweixin-1.0.0.js');
 }
 
-function addPay() {
-  var pay = $('[data-am-widget="wechatpay"]');
-  var payBtn = pay.find('.am-wechatpay-btn');
-  var options = $.AMUI.utils.parseOptions(payBtn.data('wechatPay'));
+function payHandler() {
+  if (!window.wx) {
+    return;
+  }
 
-  wx.ready(function() {
-    payBtn.on('click', function() {
-      wx.checkJsApi({
-        jsApiList: ['chooseWXPay'],
-        success: function(res) {
-          wx.chooseWXPay({
-            timestamp: options.timestamp,  // 支付签名时间戳
-            nonceStr: options.nonceStr,  // 支付签名随机串
-            package: options.package,  // 统一支付接口返回的prepay_id参数值
-            signType: options.signType, // 加密类型
-            paySign: options.paySign // 支付签名
-          });
-        },
-        fail: function() {
-          alert('你微信当前版本不支持支付功能!');
-        }
-      });
+  var $pay = $('[data-am-widget="wechatpay"]');
+  var $payBtn = $pay.find('.am-wechatpay-btn');
+  var options = $.AMUI.utils.parseOptions($payBtn.data('wechatPay'));
+  console.log(options);
+
+  $payBtn.on('click', function() {
+    wx.checkJsApi({
+      jsApiList: ['chooseWXPay'],
+      success: function(res) {
+        wx.chooseWXPay({
+          timestamp: options.timestamp,  // 支付签名时间戳
+          nonceStr: options.nonceStr,  // 支付签名随机串
+          package: options.package,  // 统一支付接口返回的prepay_id参数值
+          signType: options.signType, // 加密类型
+          paySign: options.paySign // 支付签名
+        });
+      },
+      fail: function() {
+        alert('你微信当前版本不支持支付功能!');
+      }
     });
   });
 }
 
 var payInit = function() {
-  $('.am-wechatpay').length && addPayApi(addPay);
+  $('.am-wechatpay').length && payHandler();
 };
 
 $(document).on('ready', payInit);

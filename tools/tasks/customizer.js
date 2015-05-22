3a142d53e69a215e61f317d01c7ebcd4500ac095
Minwe <minwe@yunshipei.com>
2015-05-23 00:58:33 +0800

    [fixed] fixes customize task

--- tools/tasks/customizer.js
+++ tools/tasks/customizer.js
@@ -6,11 +6,13 @@ var fs = require('fs');
 var _ = require('lodash');
 var format = require('util').format;
 var browserify = require('browserify');
-var transform = require('vinyl-transform');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var del = require('del');
 var runSequence = require('run-sequence');
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var collapser = require('bundle-collapser/plugin');
 var file = require('../lib/file');
 var cstmzPath = path.join(__dirname, '../../dist/customized');
 var cstmzTmp = path.join(__dirname, '../../.cstmz-tmp');
@@ -56,7 +58,7 @@ var less = [
   '@import "base.less";'
 ];
 var js = [
-  'require("./core");'
+  'require("core");'
 ];
 
 gulp.task('customizer:preparing', function(callback) {
@@ -65,7 +67,7 @@ gulp.task('customizer:preparing', function(callback) {
   });
 
   config.js.forEach(function(file) {
-    js.push(format('require("./%s");', file));
+    js.push(format('require("%s");', file));
   });
 
   // widgets
@@ -77,7 +79,7 @@ gulp.task('customizer:preparing', function(callback) {
     }
 
     config.widgets.forEach(function(widget) {
-      js.push(format('require("./%s");', widget.name));
+      js.push(format('require("%s/src/%s");', widget.name, widget.name));
       less.push(format('@import "../../widget/%s/src/%s.less";',
         widget.name, widget.name));
       var pkg = require(path.join('../../widget', widget.name, 'package.json'));
@@ -124,51 +126,19 @@ gulp.task('customizer:less', function() {
     .pipe($.size({showFiles: true, gzip: true, title: 'gzipped'}));
 });
 
-gulp.task('customizer:js', function(cb) {
-  runSequence(
-    [
-      'customizer:js:copy:core',
-      'customizer:js:copy:widgets'
-    ],
-    'customizer:js:browserify',
-    cb);
-});
-
-// Copy ui js files to build dir.
-gulp.task('customizer:js:copy:widgets', function() {
-  $.util.log($.util.colors.yellow('Start copy widgets js to build dir....'));
-  return gulp.src([
-    '*/src/*.js',
-    '!{layout*,blank,container}' +
-    '/src/*.js'], {cwd: './widget'})
-    .pipe($.rename(function(path) {
-      path.dirname = ''; // remove widget dir
-    }))
-    .pipe(gulp.dest(DEFAULTS.tmp + '/js'));
-});
-
-// Copy core js files to build dir.
-gulp.task('customizer:js:copy:core', function() {
-  return gulp.src('*.js', {
-    cwd: './js'
-  })
-    .pipe(gulp.dest(DEFAULTS.tmp + '/js'));
-});
-
-gulp.task('customizer:js:browserify', function() {
-  var bundler = transform(function(filename) {
-    var b = browserify({
-      entries: filename,
-      basedir: path.join(__dirname, DEFAULTS.tmp, 'js')
-    });
-    return b.bundle();
-  });
-
-  return gulp.src(DEFAULTS.js,
-    {cwd: path.join(__dirname, DEFAULTS.tmp, 'js')})
-    .pipe(bundler)
+gulp.task('customizer:js', function() {
+  return browserify({
+    entries: DEFAULTS.js,
+    paths: [path.join(__dirname, '../../js'), path.join(__dirname, '../../widget')]
+  }).plugin(collapser).bundle()
+    .pipe(source('amazeui.custom.js'))
+    .pipe(buffer())
     .pipe(gulp.dest(cstmzPath))
-    .pipe($.uglify())
+    .pipe($.uglify({
+      output: {
+        ascii_only: true
+      }
+    }))
     .pipe($.rename({
       suffix: '.min',
       extname: '.js'

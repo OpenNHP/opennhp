8c7ba30819383b03f7faf6bdf3ed8a83b36ed162
Minwe <minwe@yunshipei.com>
2015-06-24 16:05:37 +0800

    [changed] add UI.plugin function to make jQuery plugin

--- js/ui.alert.js
+++ js/ui.alert.js
@@ -16,10 +16,10 @@ var Alert = function(element, options) {
   this.options = $.extend({}, Alert.DEFAULTS, options);
   this.$element = $(element);
 
-  this.$element.
-    addClass('am-fade am-in').
-    on('click.alert.amui', '.am-close', function() {
-      _this.close.call(this);
+  this.$element
+    .addClass('am-fade am-in')
+    .on('click.alert.amui', '.am-close', function() {
+      _this.close();
     });
 };
 
@@ -28,49 +28,28 @@ Alert.DEFAULTS = {
 };
 
 Alert.prototype.close = function() {
-  var $this = $(this);
-  var $target = $this.hasClass('am-alert') ?
-    $this :
-    $this.parent('.am-alert');
+  var $element = this.$element;
 
-  $target.trigger('close.alert.amui');
-
-  $target.removeClass('am-in');
+  $element.trigger('close.alert.amui').removeClass('am-in');
 
   function processAlert() {
-    $target.trigger('closed.alert.amui').remove();
+    $element.trigger('closed.alert.amui').remove();
   }
 
-  UI.support.transition && $target.hasClass('am-fade') ?
-    $target.
-      one(UI.support.transition.end, processAlert).
-      emulateTransitionEnd(200) : processAlert();
+  UI.support.transition && $element.hasClass('am-fade') ?
+    $element
+      .one(UI.support.transition.end, processAlert)
+      .emulateTransitionEnd(200) :
+    processAlert();
 };
 
-// Alert Plugin
-$.fn.alert = function(option) {
-  return this.each(function() {
-    var $this = $(this);
-    var data = $this.data('amui.alert');
-    var options = typeof option == 'object' && option;
-
-    if (!data) {
-      $this.data('amui.alert', (data = new Alert(this, options || {})));
-    }
-
-    if (typeof option == 'string') {
-      data[option].call($this);
-    }
-  });
-};
+// plugin
+UI.plugin('alert', Alert);
 
 // Init code
 $(document).on('click.alert.amui.data-api', '[data-am-alert]', function(e) {
   var $target = $(e.target);
-  $(this).addClass('am-fade am-in');
   $target.is('.am-close') && $(this).alert('close');
 });
-
-$.AMUI.alert = Alert;
 
 module.exports = Alert;

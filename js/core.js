02bd013955c801ba398c52a9cea5100d6e443a0f
Minwe <minwe@yunshipei.com>
2015-11-20 17:10:21 +0800

    stronger image preview in Wechat, see #457

--- js/core.js
+++ js/core.js
@@ -169,6 +169,20 @@ UI.utils.generateGUID = function(namespace) {
   return uid;
 };
 
+UI.utils.getAbsoluteUrl = (function() {
+  var a;
+
+  return function(url) {
+    if (!a) {
+      a = document.createElement('a');
+    }
+
+    a.href = url;
+
+    return a.href;
+  };
+})();
+
 /**
  * Plugin AMUI Component to jQuery
  *

4a19ce34202bbb7ae6591cb0ee5d2fe6d4d55dfb
FengYi <f1@hixiu.com>
2025-06-07 10:38:29 +0800

    chore: modify comments

--- docker/nhp-app/main.go
+++ docker/nhp-app/main.go
@@ -3,18 +3,16 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	// 创建一个默认的 Gin 路由器
+	//Create a default Gin router
 	r := gin.Default()
 
-	// 定义一个 GET 路由，路径为 "/"
+	//Set the router to use the default middleware
 	r.GET("/", func(c *gin.Context) {
-
-		// 返回 JSON 响应
+		//Handle the request and respond with a JSON message
 		c.JSON(200, gin.H{
 			"message": "Hello World!",
 		})
 	})
-
-	// 启动服务器，默认监听 0.0.0.0:8080
+	// Set the router to listen on port 8080
 	r.Run()
 }

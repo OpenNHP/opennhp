c0be524776d5e322b3d0cd6b288a1f4c65576bbf
F1 <f1@hixiu.com>
2026-09-22 11:07:20 +0800

    fix(ac/ebpf): remove TTL refresh in tc_egress that prevented whitelist expiration
    tc_egress was adding entries to the spp whitelist map with a 180-second
    TTL on every egress packet, effectively refreshing the whitelist entry
    and preventing access from ever expiring. This caused the 15-second
    knock timeout to never trigger.
    
    The tc_egress program now simply passes all egress traffic. Established
    connections are tracked by the conn_track map in the XDP program, so
    egress traffic is automatically allowed without refreshing the whitelist.

--- nhp/ebpf/xdp/tc_egress.c
+++ nhp/ebpf/xdp/tc_egress.c
@@ -13,88 +13,23 @@
 #define IPPROTO_TCP 6
 #define IPPROTO_UDP 17
 #define IPPROTO_ICMP 1
-#define KEY_EXPIRE_TIME_SECONDS 180 * 1000000000ULL
 #define MAX_ENTRIES 1000000
 
-struct whitelist_key {
-    __be32 src_ip;
-    __be32 dst_ip;
-    __be16 dst_port;
-    __u8 protocol;
-} __attribute__((packed));
-
-struct whitelist_value {
-    __u8 allowed;
-    __u64 expire_time;
-};
-
-struct {
-    __uint(type, BPF_MAP_TYPE_LRU_HASH);
-    __type(key, struct whitelist_key);
-    __type(value, struct whitelist_value);
-    __uint(max_entries, MAX_ENTRIES);
-    __uint(pinning, LIBBPF_PIN_BY_NAME);
-} spp SEC(".maps");
+// tc_egress only handles egress traffic (server -> client responses).
+// It does NOT add entries to any whitelist map to avoid refreshing
+// the whitelist TTL and causing access to never expire.
+// Established connections are tracked by the conn_track map in the
+// XDP program, so egress traffic for established connections is
+// automatically allowed through.
 
 SEC("tc/egress")
 int tc_egress_prog(struct __sk_buff *ctx)
 {
-
-    void *data = (void *)(long)ctx->data;
-    void *data_end = (void *)(long)ctx->data_end;
-    struct ethhdr *eth = data;
-
-    if (data + sizeof(*eth) > data_end)
-        return TC_ACT_OK;
-
-    if (bpf_ntohs(eth->h_proto) != ETH_P_IP)
-        return TC_ACT_OK;
-
-    struct iphdr *iph = (void *)(eth + 1);
-    if ((void *)(iph + 1) > data_end)
-        return TC_ACT_OK;
-
-    if (iph->ihl < 5 || iph->version != 4)
-        return TC_ACT_OK;
-
-    __be32 src_ip = iph->saddr;
-    __be32 dst_ip = iph->daddr;
-    __u8 protocol = iph->protocol;
-
-    __be16 sport = 0, dport = 0;
-
-    if (protocol == IPPROTO_TCP) {
-        struct tcphdr *tcp = (void *)iph + (iph->ihl * 4);
-        if ((void *)(tcp + 1) > data_end)
-            return TC_ACT_OK;
-        sport = tcp->source;
-        dport = tcp->dest;
-    } else if (protocol == IPPROTO_UDP) {
-        struct udphdr *udp = (void *)iph + (iph->ihl * 4);
-        if ((void *)(udp + 1) > data_end)
-            return TC_ACT_OK;
-        sport = udp->source;
-        dport = udp->dest;
-    } else if (protocol == IPPROTO_ICMP) {
-        sport = 0;
-        dport = 0;
-    } else {
-        return TC_ACT_OK;
-    }
-
-    struct whitelist_key spp_key = {
-        .src_ip = dst_ip,
-        .dst_ip = src_ip,
-        .dst_port = sport,
-        .protocol = protocol,
-    };
-
-    struct whitelist_value spp_value = {
-        .allowed = 1,
-        .expire_time = bpf_ktime_get_ns() + KEY_EXPIRE_TIME_SECONDS,
-    };
-    bpf_map_update_elem(&spp, &spp_key, &spp_value, BPF_ANY);
-
+    // Egress traffic (server responses) for established connections
+    // is automatically allowed by the conn_track map in the XDP program.
+    // This tc_egress program simply passes all egress traffic.
+    // No whitelist updates here - doing so would refresh TTL and
+    // cause access to never expire (the 180s extension was a bug).
     return TC_ACT_OK;
 }
 
